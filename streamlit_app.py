5d1075f132a6555214864e2fa994d435ba4cc4c3
foofighterwaxwalesman <114833168+foofighterwaxwalesman@users.noreply.github.com>
2022-10-01 15:57:33 -0500

    Update streamlit_app.py
    Added allowed fruit list to smoothie picker we previously added.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
+Picker_Allowed_Fruit_list = ['Avocado','Strawberries']
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -15,7 +16,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),Picker_Allowed_Fruit_list)
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
