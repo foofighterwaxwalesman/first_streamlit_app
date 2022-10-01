e3dac5f674dd08fdd7eb1109c28636431c54f959
foofighterwaxwalesman <114833168+foofighterwaxwalesman@users.noreply.github.com>
2022-10-01 15:58:53 -0500

    Update streamlit_app.py
    Updated example pre populated fruit list for smoothies

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-Picker_Allowed_Fruit_list = ['Avocado','Strawberries']
+Picker_PrePop_Fruit_List = ['Avocado','Strawberries']
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -16,7 +16,7 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),Picker_Allowed_Fruit_list)
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),Picker_PrePop_Fruit_List)
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)
